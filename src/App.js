5d975b00fe6a27d2e1ed1e58d4191f395cc9a590
Caleb <caleb.yanghannchih93@gmail.com>
2021-10-30 05:06:17 +0800

    Boiler Plate Code for react boostrap login registration page 

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Login from '././Components/login';
+import SignUp from '././Components/signup';
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Login />
+    </div>
+    <div className="App">
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <div className="container">
+        <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-in"}>Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </div>
+    <div className="auth-wrapper">
+    <div className="auth-inner">
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path="/sign-in" component={Login} />
+        <Route path="/sign-up" component={SignUp} />
+      </Switch>
+    </div>
+  </div>
+  </Router>);
+}
+
+export default App;
